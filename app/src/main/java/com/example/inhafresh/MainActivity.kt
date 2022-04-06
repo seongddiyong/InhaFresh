f74b50b7b59cf423413984d381f69205ad2d91bb
seongddiyong <kodd1102@naver.com>
2022-04-07 00:55:08 +0900

    helped..

--- app/src/main/java/com/example/inhafresh/MainActivity.kt
+++ app/src/main/java/com/example/inhafresh/MainActivity.kt
@@ -18,21 +18,57 @@ import com.naver.maps.map.util.FusedLocationSource
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    lateinit var mapView: MapView
+
     private lateinit var locationSource: FusedLocationSource
     private lateinit var naverMap: NaverMap
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mapView = findViewById<MapView>(R.id.map_view)
+        mapView.onCreate(savedInstanceState)
+
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+
         val fm = supportFragmentManager
         val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
-            ?: MapFragment.newInstance().also {
-                fm.beginTransaction().add(R.id.map, it).commit()
-            }
+            ?: MapFragment.newInstance(NaverMapOptions().zoomControlEnabled(false))
+                .also {
+                    fm.beginTransaction().add(R.id.map, it).commit()
+                }
+        mapFragment.getMapAsync(this);
+    }
 
-        locationSource =
-            FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 
     override fun onRequestPermissionsResult(requestCode: Int,
@@ -41,6 +77,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         if (locationSource.onRequestPermissionsResult(requestCode, permissions,
                 grantResults)) {
             if (!locationSource.isActivated) { // 권한 거부됨
+                Log.d("tag", "권한 거부")
                 naverMap.locationTrackingMode = LocationTrackingMode.None
             }
             return
@@ -49,15 +86,22 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     override fun onMapReady(naverMap: NaverMap) {
+
+        Toast.makeText(this, "onMapReady 실행 중", Toast.LENGTH_SHORT).show();
+
+        this.naverMap = naverMap
         val uiSettings = naverMap.uiSettings
+        //나침반:비활, 현위치버튼:활성화
         uiSettings.isCompassEnabled = false
         uiSettings.isLocationButtonEnabled = true
 
-        this.naverMap = naverMap
         naverMap.locationSource = locationSource
+
 
         naverMap.locationTrackingMode = LocationTrackingMode.Follow
 
+
+        //좌표 변경 시 토스트로 표시
         naverMap.addOnLocationChangeListener { location ->
             Toast.makeText(this, "${location.latitude}, ${location.longitude}",
                 Toast.LENGTH_SHORT).show()
@@ -67,6 +111,4 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     companion object {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
-
-
 }
